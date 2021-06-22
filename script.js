c24b0b8d5a9e865969b0aa354f6f4f6e56b291fe
Moayad Alsariya <moayad.alsariya@gmail.com>
2021-06-22 14:59:43 +0300

    add validation

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log('working');
+// wait for DOM to load
+$(() => {
+    $("#form-vaild").validate({
+        rules: {
+            firstname: {
+                required: true,
+                minlength: 3,
+                maxlength: 150
+            },
+            lastname: {
+                required: true,
+                minlength: 3,
+                maxlength: 150
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            password: {
+                required: true,
+                minlength: 8,
+                pwcheck: true
+            },
+            confirm_password: {
+                required: true,
+                minlength: 8,
+                equalTo: '[name="password"]'
+            },
+            photo: {
+                pwcheck: true
+            },
+            terms: {
+                required: true
+            }
+        },
+        messages: {
+            password: {
+                pwcheck: "password must conain characters and numbers"
+            },
+            photo: {
+                pwcheck: "Image must be png or jpg"
+            }
+        }
+    });
+    $.validator.addMethod("pwcheck", function(value) {
+        return /^[A-Za-z0-9\d=!\-@._*]*$/.test(value) // consists of only these
+            &&
+            /[a-z]/.test(value) // has a lowercase letter
+            &&
+            /\d/.test(value) // has a digit
+    });
+    $.validator.addMethod("imgcheck", function(value) {
+        return (/\.(jpg||png)$/i).test(value);
+    });
+})
